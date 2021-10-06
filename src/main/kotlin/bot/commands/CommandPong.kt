d2dbd0635ba02399451ba9d32645658a296e90a5
Methargus <przemek.cwalina@gmail.com>
2021-10-06 21:10:12 +0200

    Tweetpik logging adjustion

--- src/main/kotlin/bot/commands/CommandPong.kt
+++ src/main/kotlin/bot/commands/CommandPong.kt
@@ -1,14 +1,7 @@
 package bot.commands
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
-import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.TextChannel
-import net.dv8tion.jda.api.events.Event
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.io.File
 
@@ -16,13 +9,6 @@ import java.io.File
 class CommandPong : Command() {
     override val name = "pong"
     private val log = LoggerFactory.getLogger(javaClass)
-
-//    override suspend fun run(event: Event) {
-//        var event = event as GuildMessageReceivedEvent
-//        var channel = event.channel
-//
-//        execute(channel, listOf())
-//    }
 
     override suspend fun execute(channel: TextChannel, args: List<String>) {
         val resourcePath = "/imno.jpg"
